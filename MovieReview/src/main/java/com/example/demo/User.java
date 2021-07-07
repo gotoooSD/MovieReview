f5851a4d1ce3a39d7ef1715700472b90be9ea296
id <id@IN0013469>
2021-07-07 16:45:07 +0900

    高地2021/07/07
    修正内容：Userの設定

--- MovieReview/src/main/java/com/example/demo/User.java
+++ MovieReview/src/main/java/com/example/demo/User.java
@@ -10,11 +10,85 @@ import javax.persistence.Table;
 @Table(name="users")
 public class User {
 	//フィールド
-	//フィールド2
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int code;
+	private String name;
+	private String gender;
+	private int age;
+	private String email;
+	private String password;
 
 	//コンストラクタ
+	public User() {
 
-	//ゲッター
+	}
+
+	public User(String name, String gender, int age, String email,String password) {
+		this.name = name;
+		this.gender = gender;
+		this.age = age;
+		this.email = email;
+		this.password = password;
+	}
+
+	public User(int code, String name, String gender, int age, String email,String password) {
+		this.code = code;
+		this.name = name;
+		this.gender = gender;
+		this.age = age;
+		this.email = email;
+		this.password = password;
+	}
+
+	//ゲッタ＆セッタのインスタンスを生成
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
 }

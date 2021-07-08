b167bcd32f2f2d0664a4f67e9be7f79da8e24e87
id <id@IN0013469>
2021-07-08 13:54:08 +0900

    8th

--- MovieReview/src/main/java/com/example/demo/MovieController.java
+++ MovieReview/src/main/java/com/example/demo/MovieController.java
@@ -6,9 +6,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -33,95 +31,95 @@ public class MovieController {
 		mv.addObject("movie", movieList);
 
 		//遷移先を指定
-		mv.setViewName("movie");
-		return mv;
-	}
-
-	/**
-	  ジャンルで映画一覧画面
-	 **/
-	//http://localhost:8080/movies
-	//ジャンルで映画一覧を表示する
-	@RequestMapping("/movies/{genre}")
-	public ModelAndView moviesGenre(
-			@RequestParam("genru") String genru,
-			ModelAndView mv) {
-
-		//検索を実行して表示
-		List<Movie> movieList = movieRepository.findByGenru(genru);
-		mv.addObject("movie", movieList);
-
-
-		//遷移先を指定
 		mv.setViewName("movies");
 		return mv;
 	}
 
-	/**
-	  国で映画一覧画面
-	 **/
-	//http://localhost:8080/movies
-	//国(洋画/邦画)で映画一覧を表示する
-	@RequestMapping("/movies/{country}")
-	public ModelAndView moviesCountry(
-			@RequestParam("country") String country,
-			ModelAndView mv) {
+//	/**
+//	  ジャンルで映画一覧画面
+//	 **/
+//	//http://localhost:8080/movies
+//	//ジャンルで映画一覧を表示する
+//	@RequestMapping("/movies/{genre}")
+//	public ModelAndView moviesGenre(
+//			@RequestParam("genru") String genru,
+//			ModelAndView mv) {
+//
+//		//検索を実行して表示
+//		List<Movie> movieList = movieRepository.findByGenru(genru);
+//		mv.addObject("movie", movieList);
+//
+//
+//		//遷移先を指定
+//		mv.setViewName("movies");
+//		return mv;
+//	}
 
-		//検索を実行して表示
-		List<Movie> movieList = movieRepository.findByCountry(country);
-		mv.addObject("movie", movieList);
-
-		//遷移先を指定
-		mv.setViewName("movies");
-		return mv;
-	}
-
-	/**
-	  映画の検索結果の一覧画面
-	 **/
-	//http://localhost:8080/movies/kensaku
-	//映画の検索結果を一覧で表示する
-	@PostMapping("/movies/kensaku")
-	public ModelAndView moviesKensaku(ModelAndView mv) {
-
-
-		//遷移先を指定
-		mv.setViewName("movies");
-		return mv;
-	}
-
-	/**
-	  映画の並び替えの一覧画面
-	 **/
-	//http://localhost:8080/movies/sort
-	//映画の並び替えをして一覧で表示する
-	@PostMapping("/movies/sort")
-	public ModelAndView moviesSort(ModelAndView mv) {
-		//全件検索を実行して表示
-				List<Movie> movieList = movieRepository.findAll();
-				mv.addObject("movie", movieList);
-
-
-		//遷移先を指定
-		mv.setViewName("movies");
-		return mv;
-	}
-
-	/**
-	  全映画の一覧画面
-	 **/
-	//http://localhost:8080/movies
-	//全映画の一覧を表示する
-	@RequestMapping("/movies/{moviecode}")
-	public ModelAndView moviesCode(ModelAndView mv) {
-
-		//全件検索を実行して表示
-		List<Movie> movieList = movieRepository.findAll();
-		mv.addObject("movie", movieList);
-
-		//遷移先を指定
-		mv.setViewName("movie");
-		return mv;
-	}
+//	/**
+//	  国で映画一覧画面
+//	 **/
+//	//http://localhost:8080/movies
+//	//国(洋画/邦画)で映画一覧を表示する
+//	@RequestMapping("/movies/{country}")
+//	public ModelAndView moviesCountry(
+//			@RequestParam("country") String country,
+//			ModelAndView mv) {
+//
+//		//検索を実行して表示
+//		List<Movie> movieList = movieRepository.findByCountry(country);
+//		mv.addObject("movie", movieList);
+//
+//		//遷移先を指定
+//		mv.setViewName("movies");
+//		return mv;
+//	}
+//
+//	/**
+//	  映画の検索結果の一覧画面
+//	 **/
+//	//http://localhost:8080/movies/kensaku
+//	//映画の検索結果を一覧で表示する
+//	@PostMapping("/movies/kensaku")
+//	public ModelAndView moviesKensaku(ModelAndView mv) {
+//
+//
+//		//遷移先を指定
+//		mv.setViewName("movies");
+//		return mv;
+//	}
+//
+//	/**
+//	  映画の並び替えの一覧画面
+//	 **/
+//	//http://localhost:8080/movies/sort
+//	//映画の並び替えをして一覧で表示する
+//	@PostMapping("/movies/sort")
+//	public ModelAndView moviesSort(ModelAndView mv) {
+//		//全件検索を実行して表示
+//				List<Movie> movieList = movieRepository.findAll();
+//				mv.addObject("movie", movieList);
+//
+//
+//		//遷移先を指定
+//		mv.setViewName("movies");
+//		return mv;
+//	}
+//
+//	/**
+//	  全映画の一覧画面
+//	 **/
+//	//http://localhost:8080/movies
+//	//全映画の一覧を表示する
+//	@RequestMapping("/movies/{moviecode}")
+//	public ModelAndView moviesCode(ModelAndView mv) {
+//
+//		//全件検索を実行して表示
+//		List<Movie> movieList = movieRepository.findAll();
+//		mv.addObject("movie", movieList);
+//
+//		//遷移先を指定
+//		mv.setViewName("movie");
+//		return mv;
+//	}
 
 }
--- MovieReview/src/main/java/com/example/demo/MovieRepository.java
+++ MovieReview/src/main/java/com/example/demo/MovieRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Integer>{
 
-	List<Movie> findByGenru(String genru);
+	List<Movie> findByGenre(String genru);
 
 	List<Movie> findByCountry(String country);
 

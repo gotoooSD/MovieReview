577096ce58b8f867c49e6810d1c094f4ac9a0ea1
id <id@IN0013470>
2021-07-08 14:10:45 +0900

    12th ログイン画面までいけます

--- MovieReview/src/main/java/com/example/demo/ReviewRepository.java
+++ MovieReview/src/main/java/com/example/demo/ReviewRepository.java
@@ -1,8 +1,9 @@
 package com.example.demo;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ReviewRepository {
+public interface ReviewRepository extends JpaRepository<Review, Integer>{
 
 }

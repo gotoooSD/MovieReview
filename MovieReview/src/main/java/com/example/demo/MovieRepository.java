299ee43c86d5122b35050f4951cccb7180e7e4ff
id <id@IN0013470>
2021-07-09 11:05:36 +0900

    マージ後

--- MovieReview/src/main/java/com/example/demo/MovieRepository.java
+++ MovieReview/src/main/java/com/example/demo/MovieRepository.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Integer>{
+	List<Movie> findByTitleLike(String keyword);
+
+	List<Movie> findByTitleLikeAndGenre(String keyword, String genre);
 
 	List<Movie> findByGenre(String genre);
 

e8d0cca0d1b5de184dd8d338796d82882a2e6b18
id <id@IN0013469>
2021-07-14 13:54:33 +0900

    ヘルプ画面作成完了

--- MovieReview/src/main/java/com/example/demo/HelpRepository.java
+++ MovieReview/src/main/java/com/example/demo/HelpRepository.java
@@ -1,7 +1,12 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HelpRepository extends JpaRepository<Help, Integer>{
+
+	List<Help> findByCode(int code);
+
 
 }
